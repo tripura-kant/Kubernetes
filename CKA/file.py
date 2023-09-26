dbfa3b916efc0caf92aa2070a1bb5d1e6ed6cc33
Tripura kant <tripura.kant@zoom.us>
2023-09-27 01:00:03 +0530

    ok

--- CKA/file.py
+++ CKA/file.py
@@ -1,4 +1,4 @@
 for i in range(1, 50):
-    file_name = f"question{i}.yaml"
+    file_name = f"question{i}.py"
     with open(file_name, "w") as file:
         file.write("# This is file " + file_name)
